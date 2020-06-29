aee2e508d311931b78bbe391adc2229923d51d33
Matthew Jones <mail@matthewrjones.me>
2020-06-29 16:56:28 -0400

    Added method to perform game searches

--- game/game.data.go
+++ game/game.data.go
@@ -36,7 +36,7 @@ func getAllGames(resultsPerPage int, page int) ([]Game, error) {
 		ORDER BY global_sales desc
 		LIMIT ?, ?;`, limitStart, limitEnd)
 
-	return handleDatabaseResults(results, err)
+	return processDatabaseResults(results, err)
 }
 
 func getSingleGameByRank(rank int) ([]Game, error) {
@@ -58,10 +58,44 @@ func getSingleGameByRank(rank int) ([]Game, error) {
 		FROM games
 		WHERE games.rank = ?`, rank)
 
-	return handleDatabaseResults(results, err)
+	return processDatabaseResults(results, err)
 }
 
-func handleDatabaseResults(results *sql.Rows, err error) ([]Game, error) {
+func searchForGames(name string, platform string, genre string, publisher string, minSales string, maxSales string) ([]Game, error) {
+	// Build the query string and collect arguments.
+	firstConditionAdded := false
+	arguments := []interface{}{}
+
+	sql := "SELECT games.rank, name, platform, year, genre, publisher, na_sales, eu_sales, jp_sales, other_sales, global_sales FROM games WHERE "
+
+	addSearchConditional("name", name, "=", &firstConditionAdded, &sql, &arguments)
+	addSearchConditional("platform", platform, "=", &firstConditionAdded, &sql, &arguments)
+	addSearchConditional("genre", genre, "=", &firstConditionAdded, &sql, &arguments)
+	addSearchConditional("publisher", publisher, "=", &firstConditionAdded, &sql, &arguments)
+	addSearchConditional("minSales", minSales, ">", &firstConditionAdded, &sql, &arguments)
+	addSearchConditional("maxSales", maxSales, "<", &firstConditionAdded, &sql, &arguments)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	results, err := database.DbConn.QueryContext(ctx, sql, arguments...)
+
+	return processDatabaseResults(results, err)
+}
+
+func addSearchConditional(argumentName string, argument string, operator string, firstConditionAdded *bool, sql *string, arguments *[]interface{}) {
+	if argument != "" {
+		if *firstConditionAdded {
+			*sql += " AND "
+		}
+		*sql += argumentName + " " + operator + " ?"
+		*arguments = append(*arguments, argument)
+
+		*firstConditionAdded = true
+	}
+}
+
+func processDatabaseResults(results *sql.Rows, err error) ([]Game, error) {
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err

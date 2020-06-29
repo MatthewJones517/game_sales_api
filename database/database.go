5978ea34882b2014b152a70c8c91560b55cb4462
Matthew Jones <mail@matthewrjones.me>
2020-06-29 12:14:30 -0400

    Created database package.

---
+++ database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+// DbConn is the variable the contains the database connection.
+var DbConn *sql.DB
+
+// SetupDatabase prepares the database connection for use.
+func SetupDatabase() {
+	// Pull in connection data from env variables
+	err := godotenv.Load("sensitive.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	mysqlUser := os.Getenv("MYSQL_USER")
+	mysqlPass := os.Getenv("MYSQL_PASS")
+	mysqlHost := os.Getenv("MYSQL_HOST")
+	mysqlDbnm := os.Getenv("MYSQL_DBNM")
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s)/%s", mysqlUser, mysqlPass, mysqlHost, mysqlDbnm)
+
+	DbConn, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	DbConn.SetMaxOpenConns(3)
+	DbConn.SetMaxIdleConns(3)
+	DbConn.SetConnMaxLifetime(60 * time.Second)
+}

8b6b9e64e6afe903448f051a0fd890fbdf6052ea
Matthew Jones <mail@matthewrjones.me>
2020-06-29 14:28:10 -0400

    Added method to query all game data

---
+++ game/game.data.go
@@ -0,0 +1,53 @@
+package game
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/MatthewJones517/game_sales_api/database"
+)
+
+func getAllGames(resultsPerPage int, page int) ([]Game, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	if page == 0 {
+		page = 1
+	}
+
+	limitStart := resultsPerPage * (page - 1)
+	limitEnd := resultsPerPage * page
+
+	results, err := database.DbConn.QueryContext(ctx, `SELECT 
+			games.rank, 
+			name, 
+			platform, 
+			year, 
+			genre, 
+			publisher, 
+			na_sales, 
+			eu_sales, 
+			jp_sales, 
+			other_sales, 
+			global_sales
+		FROM games LIMIT ?, ?;`, limitStart, limitEnd)
+
+	if err != nil {
+		log.Println(err.Error())
+		return nil, err
+	}
+
+	defer results.Close()
+
+	games := make([]Game, 0)
+
+	for results.Next() {
+		var game Game
+		results.Scan(&game.Rank, &game.Name, &game.Platform, &game.Year, &game.Genre, &game.Publisher, &game.NASales, &game.EUSales, &game.JPSales, &game.OtherSales, &game.GlobalSales)
+
+		games = append(games, game)
+	}
+
+	return games, nil
+}

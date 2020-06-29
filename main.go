93d7a498ff77f1fbe69fa616300a938510e2517b
Matthew Jones <mail@matthewrjones.me>
2020-06-29 19:00:32 -0400

    Added root handler

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ func main() {
 	publisher.SetupRoutes()
 	game.SetupRoutes()
 
+	rootHandler := http.HandlerFunc(handleRoot)
+	http.Handle("/", rootHandler)
+
 	fmt.Println(("Now listening on port 5050."))
 	log.Fatal(http.ListenAndServe(":5050", nil))
+}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><head><title>Game Sales Data API</title></head><body>This is an API meant to be consumed with something like <a href='https://www.postman.com/'>Postman</a>. Check out the <a href='https://github.com/MatthewJones517/game_sales_api'>Github Repo</a> for more information.</body>")
 }

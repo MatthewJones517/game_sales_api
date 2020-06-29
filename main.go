d641cc303b30e729e63f72c7bef18e04cb437ea5
Matthew Jones <mail@matthewrjones.me>
2020-06-29 13:40:50 -0400

    Added genre routing to main method

--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
 	"net/http"
 
 	"github.com/MatthewJones517/game_sales_api/database"
+	"github.com/MatthewJones517/game_sales_api/genre"
 	"github.com/MatthewJones517/game_sales_api/platform"
 )
 
 func main() {
 	database.SetupDatabase()
 	platform.SetupRoutes()
+	genre.SetupRoutes()
 
 	fmt.Println(("Now listening on port 5050."))
 	log.Fatal(http.ListenAndServe(":5050", nil))

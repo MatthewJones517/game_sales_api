1cfd74873489c8ab4f7771bbe77134b9ca34ea4f
Matthew Jones <mail@matthewrjones.me>
2020-06-29 13:53:10 -0400

    Added publisher routing to main.

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"github.com/MatthewJones517/game_sales_api/database"
 	"github.com/MatthewJones517/game_sales_api/genre"
 	"github.com/MatthewJones517/game_sales_api/platform"
+	"github.com/MatthewJones517/game_sales_api/publisher"
 )
 
 func main() {
 	database.SetupDatabase()
 	platform.SetupRoutes()
 	genre.SetupRoutes()
+	publisher.SetupRoutes()
 
 	fmt.Println(("Now listening on port 5050."))
 	log.Fatal(http.ListenAndServe(":5050", nil))

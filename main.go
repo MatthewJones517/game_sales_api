1adcab3eeb5d32246167bee4de4061ce19992669
Matthew Jones <mail@matthewrjones.me>
2020-06-29 13:22:37 -0400

    Added routing for platform endpoint

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 
 	"github.com/MatthewJones517/game_sales_api/database"
+	"github.com/MatthewJones517/game_sales_api/platform"
 )
 
 func main() {
 	database.SetupDatabase()
+	platform.SetupRoutes()
+
 	fmt.Println(("Now listening on port 5050."))
 	log.Fatal(http.ListenAndServe(":5050", nil))
 }
